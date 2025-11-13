866bd2db88e24a5423899279a5aa752a6054941d
romsper <r.speranskii@gmail.com>
2025-11-13 01:50:47 -0500

    Migrated to JUnit 6

--- src/main/kotlin/frontend/helpers/BaseTest.kt
+++ src/main/kotlin/frontend/helpers/BaseTest.kt
@@ -1,7 +1,6 @@
 package frontend.helpers
 
 import com.codeborne.selenide.Selenide
-import com.codeborne.selenide.Selenide.sleep
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
--- src/main/kotlin/general/listeners/TestListener.kt
+++ src/main/kotlin/general/listeners/TestListener.kt
@@ -11,23 +11,23 @@ import org.junit.platform.launcher.TestPlan
 
 class TestListener : TestExecutionListener {
 
-    override fun testPlanExecutionStarted(testPlan: TestPlan?) {
+    override fun testPlanExecutionStarted(testPlan: TestPlan) {
         println("|----- START -----|\n")
         println("Initializing RemoteWebDriver or run in LocalBrowser...").also { InitDriverProvider() } // Read details in InitDriverProvider.kt
     }
 
-    override fun executionFinished(testIdentifier: TestIdentifier?, testExecutionResult: TestExecutionResult?) {
-        if (testIdentifier!!.isTest) println("Finish: ${testIdentifier.displayName}")
-        if (testExecutionResult!!.status == TestExecutionResult.Status.FAILED && testIdentifier.displayName != "JUnit Jupiter") {
+    override fun executionFinished(testIdentifier: TestIdentifier, testExecutionResult: TestExecutionResult) {
+        if (testIdentifier.isTest) println("Finish: ${testIdentifier.displayName}")
+        if (testExecutionResult.status == TestExecutionResult.Status.FAILED && testIdentifier.displayName != "JUnit Jupiter") {
             attachScreenshot()
         }
     }
 
-    override fun executionSkipped(testIdentifier: TestIdentifier?, reason: String?) {
-        if (testIdentifier!!.isTest) println("Skip: ${testIdentifier.displayName}\nReason: $reason")
+    override fun executionSkipped(testIdentifier: TestIdentifier, reason: String) {
+        if (testIdentifier.isTest) println("Ignore: ${testIdentifier.displayName}\nReason: $reason")
     }
 
-    override fun testPlanExecutionFinished(testPlan: TestPlan?) {
+    override fun testPlanExecutionFinished(testPlan: TestPlan) {
         Selenide.closeWebDriver()
         println("|----- FINISH -----|")
     }
